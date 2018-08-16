5ca6116358b0329c6660eb9c6341df4c53564d7b
Unknown <tjbashiribadu@gmail.com>
2018-08-16 23:07:41 +0400

    Vue.js configured

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,4 +8,34 @@
 
 @section('content')
     <p>You are logged in!</p>
+    vue?
+    <example-component></example-component>
+@stop
+
+@section('js')
+    <script> console.log('Hello world!'); </script>
+
+    <script>
+        {{----}}
+        console.log('entered');
+        window.app = new Vue({
+            el: '#app',
+            data: {
+                msg: 'hello'
+            },
+            created(){
+                console.log('vue created');
+                Echo.private('message-channel')
+                    .listen('Message', (e) => {
+                    alert("private message event trigged", e);
+                console.log(e);
+            });
+                //Echo.channel('broadcast-channel')
+                // .listen('Message', (e) => {
+                //     alert("broadcast channel message event trigged", e);
+                //     console.log(e);
+                // });
+            }
+        });
+    </script>
 @stop
--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title_prefix', config('adminlte.title_prefix', ''))
-@yield('title', config('adminlte.title', 'AdminLTE 2'))
-@yield('title_postfix', config('adminlte.title_postfix', ''))</title>
+    @yield('title', config('adminlte.title', 'AdminLTE 2'))
+    @yield('title_postfix', config('adminlte.title_postfix', ''))</title>
+    <!-- csrf token for laravel broadcasting package requirement-->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
@@ -60,6 +63,8 @@
     <!-- ChartJS -->
     <script src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.min.js"></script>
 @endif
+
+<script src="{{ asset('js/app.js') }}" charset="utf-8"></script>
 
 @yield('adminlte_js')
 
