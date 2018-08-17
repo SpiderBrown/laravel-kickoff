6bb1e810720a434cba7454235127922d3220005d
Unknown <tjbashiribadu@gmail.com>
2018-08-17 22:23:21 +0400

    ui flash notification added

--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -224,6 +224,28 @@
                     <!-- Main content -->
                     <section class="content">
 
+                        @if (session('success'))
+                            <div class="alert alert-success alert-dismissible show">
+                                {{ session('success') }}
+                            </div>
+                        @endif
+                        @if(session('info'))
+                            <div class="alert alert-info alert-dismissible">
+                                {{ session('info') }}
+                            </div>
+                        @endif
+                        @if(session('warning'))
+                            <div class="alert alert-warning alert-dismissible ">
+                                {{ session('warning') }}
+                            </div>
+                        @endif
+
+                        @if(session('danger'))
+                            <div class="alert alert-danger alert-dismissible show">
+                                {{ session('danger') }}
+                            </div>
+                        @endif
+
                         @yield('content')
 
                     </section>
