da46b74b0eda2319ef28251e665a93d1bf114661
Ahmed <tjbashiribadu@gmail.com>
2018-08-20 17:12:45 +0400

    add broadcast notification

---
+++ app/Notifications/AdminBroadcastMessage.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class AdminBroadcastMessage extends Notification
+{
+    use Queueable;
+
+    public $data;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data=$data;/**/
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['broadcast'];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toBroadcast($notifiable)
+    {
+        return [
+            'title' => 'Urgent Attention',
+            'message' => 'Hello Administrators, Server is down and we need your attantion',
+            'data' => $this->data
+        ];
+    }
+}
