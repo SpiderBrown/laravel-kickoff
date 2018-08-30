37a05594976586e74ddfc5916af6eacff1f1d756
Ahmed <tjbashiribadu@gmail.com>
2018-08-31 00:43:46 +0400

    complete test module

--- Modules/TestModule/Resources/views/index.blade.php
+++ Modules/TestModule/Resources/views/index.blade.php
@@ -1,4 +1,17 @@
-@extends('testmodule::layouts.master')
+@extends('adminlte::page')
+
+@section('title', config('testmodule.name') )
+
+@section('content_header')
+    <h1 class="hidden-sm hidden-xs">
+        Module <small>{{ config('testmodule.name') }}</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="{{ route('home') }}"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="{{ strtolower(config('testmodule.name'))  }}">{{ config('testmodule.name') }}</a></li>
+        <li class="active">Index</li>
+    </ol>
+@stop()
 
 @section('content')
     <h1>Hello World</h1>
